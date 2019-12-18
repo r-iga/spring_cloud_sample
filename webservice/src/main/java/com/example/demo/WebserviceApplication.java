f058a32a4ebcbeb9f4e8cd823b104d9f370b2dbc
r-iga <iga@primebrains.co.jp>
2019-12-18 15:56:33 +0900

    初回作成

---
+++ webservice/src/main/java/com/example/demo/WebserviceApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableZuulProxy
+public class WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceApplication.class, args);
+	}
+
+}
